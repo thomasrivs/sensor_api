602e807c04387ae7f5850e1c24ce237499b963f1
j <j>
2024-01-18 08:41:12 +0100

    added malfunctions and break

--- fake_app_data/sensor.py
+++ fake_app_data/sensor.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import date
+from datetime import date, timedelta
 
 import numpy as np
 
@@ -12,12 +12,20 @@ class VisitSensor:
     a particular door on a given date
     """
 
-    def __init__(self, avg_visit: int, std_visit: int) -> None:
+    def __init__(
+        self,
+        avg_visit: int,
+        std_visit: int,
+        perc_break: float = 0.015,
+        perc_malfunction: float = 0.035,
+    ) -> None:
         """Intialize sensor"""
         self.avg_visit = avg_visit
         self.std_visit = std_visit
+        self.perc_malfunction = perc_malfunction
+        self.perc_break = perc_break
 
-    def simulate_visit(self, business_date: date) -> int:
+    def simulate_visit_count(self, business_date: date) -> int:
         """Simulate the number of person detected by the sensor
         during the day"""
 
@@ -43,6 +51,27 @@ class VisitSensor:
         # Return an integer
         return np.floor(visit)
 
+    def get_visit_count(self, business_date: date) -> int:
+        """return the number of person detected by the sensor
+        during the day"""
+
+        np.random.seed(seed=business_date.toordinal())
+        proba_malfunction = np.random.random()
+
+        # The sensor can break sometimes
+        if proba_malfunction < self.perc_break:
+            print("break")
+            return 0
+
+        visit = self.simulate_visit_count(business_date)
+
+        # The sensor can also malfunction
+        if proba_malfunction < self.perc_malfunction:
+            print("malfunction")
+            visit = np.floor(visit * 0.2)  # make it so bad we can detect it ;)
+
+        return visit
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -52,4 +81,14 @@ if __name__ == "__main__":
     queried_date = date(year, month, day)
 
     capteur = VisitSensor(1500, 150)
-    print(capteur.simulate_visit(date(year=2023, month=10, day=25)))
+
+    # ad hoc test to quickly verify malfunction and break
+    init_date = date(2022, 1, 1)
+    while init_date < date(2024, 1, 1):
+        init_date += timedelta(days=1)
+        visit_count = capteur.get_visit_count(init_date)
+        print(init_date, visit_count)
+
+    # python sensor.py > log.txt
+    # cat log.txt | grep break
+    # cat log.txt | grep malfunction
